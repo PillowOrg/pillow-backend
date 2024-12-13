4e890b3be59517341e645c94b8180d0eb8cf548f
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-12-14 01:06:51 +0200

    hide django secret key

--- pillow_backend/pillow_backend/settings.py
+++ pillow_backend/pillow_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-htq#e5v-=jq!uw4i^a&o^n^byy61nrch*_u_w+x&1hvp0_-fmb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
