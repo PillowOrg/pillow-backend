720bf2bdcafae459b79d5a9c4badca75d598c3a7
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-12-14 00:30:54 +0200

    add allowed hosts

--- pillow_backend/pillow_backend/settings.py
+++ pillow_backend/pillow_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-htq#e5v-=jq!uw4i^a&o^n^byy61nrch*_u_w+x&1hvp0_-fmb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", '127.0.0.1']
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", '127.0.0.1', 'pillow.onrender.com']
 
 
 # Application definition
@@ -143,7 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
